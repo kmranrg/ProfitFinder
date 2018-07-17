267a3ad071e55cc0612929fb51776220b899d3b3
Kumar Anurag <kmranrg@gmail.com>
2018-07-17 23:59:20 +0530

    removed duplicate elements from title list

--- profit.py
+++ profit.py
@@ -1,9 +1,19 @@
 from csv import reader,writer
 from re import findall
+from collections import OrderedDict
 
 print("\t\t\t***** SUPER MARKET *****\n")
 
-data = open("records.txt").read()
+file = open("records.txt")
+data = file.read()
+file.close()
+
+title_pat = "(\S+):\s"
+title = findall(title_pat,data)
+# for removing the duplicate entries from list with order maintained
+title = list(OrderedDict.fromkeys(title))
+title.append("Profit")
+print(title)
 
 pro_pat = "Product:\s(\w+)"
 pro = findall(pro_pat,data)
@@ -21,5 +31,10 @@ sp_pat = "SP:\s(\d+)"
 sp = findall(sp_pat,data)
 print(sp)
 
+csv_file = open("tables.csv","w",newline = "")
+csv_obj = writer(csv_file)
+csv_obj.writerow(title)
+                
+csv_file.close()
 
 
